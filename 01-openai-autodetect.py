1b37ca282d0c62bade64ec0e17b4d93c9f12372e
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-08-31 01:31:40 +0800

    response structure

--- 01-openai-autodetect.py
+++ 01-openai-autodetect.py
@@ -11,10 +11,20 @@ deployment_name = "gpt-4.1-mini"
 # token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://ai.azure.com/.default")
 
 client = OpenAI()
+query = input("> Enter query: ")
 
 response = client.responses.create(
     model=deployment_name,
-    input="What is the capital of France?",
+    input=query
 )
 
 print(f"answer: {response.output[0].content[0].text}")
+
+print("--------\n\n")
+
+# response structure
+
+print(f"Response: {response.output_text}")
+print(f"Response ID: {response.id}")
+print(f"Token used: {response.usage.total_tokens}")
+print(f"Status: ", {response.status})
